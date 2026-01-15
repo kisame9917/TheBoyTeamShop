0609923f16cc26b4bb99a440ce777b1bc66f600f
Duyquyet2001 <h3ro0812@gmail.com>
2026-01-15 23:30:17 +0700

    be + fe pgg

--- vest-project-base/be/vest-backend/src/main/java/com/vestshop/Config/SecurityConfig.java
+++ vest-project-base/be/vest-backend/src/main/java/com/vestshop/Config/SecurityConfig.java
@@ -15,14 +15,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                // 🔥 QUAN TRỌNG NHẤT
+
                 .cors(Customizer.withDefaults())
 
-                // Tạm thời tắt csrf cho FE dev
+
                 .csrf(csrf -> csrf.disable())
 
                 .authorizeHttpRequests(auth -> auth
-                        // 🔥 CHO PHÉP PREFLIGHT
+
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyRequest().permitAll()
                 );
