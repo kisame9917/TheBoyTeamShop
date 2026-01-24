15f1436bc58b5f7661e8db062829a5e6b8da4fef
tuan02032003 <gauzyn3@gmail.com>
2026-01-24 15:39:20 +0700

    them anh cho sp

--- vest-project-base/be/vest-backend/src/main/java/com/vestshop/Config/WebConfig.java
+++ vest-project-base/be/vest-backend/src/main/java/com/vestshop/Config/WebConfig.java
@@ -1,18 +1,19 @@
-//package com.vestshop.Config;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//    @Value("${app.upload-dir}")
-//    private String uploadDir;
-//
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/uploads/**")
-//                .addResourceLocations("file:" + uploadDir + "/");
-//    }
-//}
+package com.vestshop.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        Path uploadDir = Paths.get("./uploads");
+        String uploadPath = uploadDir.toFile().getAbsolutePath();
+
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:/" + uploadPath + "/");
+    }
+}
