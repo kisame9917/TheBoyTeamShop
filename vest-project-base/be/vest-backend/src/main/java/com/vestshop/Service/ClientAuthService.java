d44702e9f3c8d2ba584ea466c42eb85271501e12
Duyquyet2001 <h3ro0812@gmail.com>
2026-03-03 14:40:59 +0700

    be client them quen mat khau

--- vest-project-base/be/vest-backend/src/main/java/com/vestshop/Service/ClientAuthService.java
+++ vest-project-base/be/vest-backend/src/main/java/com/vestshop/Service/ClientAuthService.java
@@ -6,4 +6,9 @@ import com.vestshop.dto.response.LoginResponse;
 
 public interface ClientAuthService {
     ClientLoginResponse login(LoginRequest req);
+    void sendOtp(String email);
+    void resetPassword(String email, String otp, String newPassword);
+    String genOtp6();
+    String hashOtp(String emailLower, String otp);
+    void verifyOtp(String email, String otp);
 }
