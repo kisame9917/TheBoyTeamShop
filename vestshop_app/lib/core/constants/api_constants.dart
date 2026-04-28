795255e326ed839bfbe367dfd4d0e5701656f959
kisame9917 <nhuthang510@gmail.com>
2026-04-28 15:20:53 +0700

    c

--- vestshop_app/lib/core/constants/api_constants.dart
+++
@@ -1,5 +0,0 @@
-class ApiConstants {
-  static const String serverUrl = 'http://10.0.2.2:8080';
-  static const String baseUrl = '$serverUrl/api';
-  static const String wsUrl = 'ws://10.0.2.2:8080/ws';
-}
